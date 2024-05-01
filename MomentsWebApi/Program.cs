7487927d8ec49187b5e942ba1b50e416506538c7
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-05-01 09:48:09 -0300

    Habilitando o Serviço de Cors para permitir consumo do Angular no front

--- MomentsWebApi/Program.cs
+++ MomentsWebApi/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
@@ -6,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Habilitando o serviço no container de Cors para permitir que o serviço Angular consuma a API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .WithHeaders(HeaderNames.ContentType);
+                      });
+});
+
+// Dependency Injection
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
@@ -17,6 +38,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Adicionando o middleware de Cors no pipeline da requisição para ser aceito requisições do endereços registrados na politica do cors informado
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
